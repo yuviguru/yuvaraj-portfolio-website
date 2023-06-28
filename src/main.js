0f1d0c5ce6f80486713849bbe5ad2b30245adb26
Yuvaraj <yuvaguru.guru8@gmail.com>
2023-06-28 22:43:01 +0530

    Remove unused dependencies

--- src/main.js
+++ src/main.js
@@ -4,18 +4,12 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-import Flicking from "@egjs/vue3-flicking";
-import "@egjs/vue3-flicking/dist/flicking.css";
-// Or, if you have to support IE9
-import "@egjs/vue3-flicking/dist/flicking-inline.css";
-
 // Import Bootstrap and BootstrapVue CSS files (order is important)
 import 'bootstrap/dist/css/bootstrap.css'
 
 const app = createApp(App)
 
 app.use(router)
-app.component("FlickingComponent", Flicking);
 
 app.mount('#app')
 
--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,4 @@
-export function chainWebpack(config) {
-    config.module
-        .rule("Snap")
-        // eslint-disable-next-line no-undef
-        .test(require.resolve("snapsvg/dist/snap.svg.js"))
-        .use("imports-loader")
-        .loader("imports-loader?this=>window,fix=>module.exports=0");
-
-    config.resolve.alias.set("snapsvg", "snapsvg/dist/snap.svg.js");
+/* eslint-env node */
+module.exports = {
+    publicPath: '/yuvaraj-portfolio-website/'
 }
